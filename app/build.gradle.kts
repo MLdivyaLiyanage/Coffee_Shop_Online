8e395f7cc352757154eb0056c904ec5d92a67e86
DELL-PC <divyaMliyanage@gmail.com>
2025-01-04 09:46:04 +0530

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.coffee_shop_online"
-        minSdk = 24
+        minSdk = 35
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
